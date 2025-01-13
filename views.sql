fb8c67ddc68bfbf7c75dd59f0ef8b778dd6071f5
Harsh Jaiswal <harshjaiswal97@gmail.com>
2025-01-14 00:33:07 +0100

    Schema

--- views.sql
+++ views.sql
@@ -2,4 +2,4 @@ CREATE VIEW vw_ChurnData AS
 SELECT * FROM prod_Churn WHERE Customer_Status IN ('Churned', 'Stayed');
 
 CREATE VIEW vw_JoinData AS
-SELECT * FROM prod_Churn WHERE Customer_Status = 'Joined';
+SELECT * FROM prod_Churn WHERE Customer_Status = 'Joined';v

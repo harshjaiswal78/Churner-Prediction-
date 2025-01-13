a877968fa0d39fce3ae01deb61fae1cb32f3da27
Harsh Jaiswal <harshjaiswal97@gmail.com>
2025-01-14 00:31:00 +0100

    Schemas

--- exploration.sql
+++ exploration.sql
@@ -1,1 +1,21 @@
+SELECT Gender, Count(Gender) as TotalCount,
+Count(Gender) * 1.0 / (Select Count(*) from stg_Churn)  as Percentage
+FROM stg_Churn
+GROUP BY Gender;
+
+SELECT Contract, Count(Contract) as TotalCount,
+Count(Contract) * 1.0 / (Select Count(*) from stg_Churn)  as Percentage
+FROM stg_Churn
+GROUP BY Contract;
+
+SELECT Customer_Status, Count(Customer_Status) as TotalCount, Sum(Total_Revenue) as TotalRev,
+Sum(Total_Revenue) / (Select sum(Total_Revenue) from stg_Churn) * 100  as RevPercentage
+FROM stg_Churn
+GROUP BY Customer_Status;
+
+SELECT State, Count(State) as TotalCount,
+Count(State) * 1.0 / (Select Count(*) from stg_Churn)  as Percentage
+FROM stg_Churn
+GROUP BY State
+ORDER BY Percentage DESC;
 
